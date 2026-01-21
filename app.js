6b5f2cd9e0dfa46b3fc7d323068066f5f35b8762
Ritish Saini <ritishsaini2727@gmail.com>
2026-01-21 21:19:06 +0530

    Initial backend setup for HRMS Lite

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import attendanceRoutes from "./routes/attendanceRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/employees", employeeRoutes);
